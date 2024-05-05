b06b979e9d42d3da7b8a9cc7a1c0e8c07af95475
Luis Vega <l2vega@live.com>
2024-05-04 23:49:54 -0600

    Cumpliendo con las el desafio se desarrolla un codigo para trabajar con API en este caso al igual que la propueta de la profesora se trabaja con un Api de libros

--- src/main/java/com/aluracursos/Bookteca/BooktecaApplication.java
+++ src/main/java/com/aluracursos/Bookteca/BooktecaApplication.java
@@ -1,17 +1,23 @@
 package com.aluracursos.Bookteca;
 
+import com.aluracursos.Bookteca.Principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BooktecaApplication {
+public class BooktecaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooktecaApplication.class, args);
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.ejecutaMenu();
 
 
-
+	}
 }
--- src/main/java/com/aluracursos/Bookteca/Principal/Principal.java
+++ src/main/java/com/aluracursos/Bookteca/Principal/Principal.java
@@ -1,14 +1,19 @@
 package com.aluracursos.Bookteca.Principal;
 
+import com.aluracursos.Bookteca.model.Datos;
 import com.aluracursos.Bookteca.service.ConsumoAPI;
+import com.aluracursos.Bookteca.service.CovierteDatos;
 import com.aluracursos.Bookteca.service.IConvierteDatos;
 
 public class Principal {
-    private static final String URL_BASE;
-    private ConsumoAPI = new ConsumoAPI();
-    private IConvierteDatos = new IConvierteDatos();
+    private static final String URL_BASE = "http://gutendex.com/books/";
+    private ConsumoAPI consumoAPI= new ConsumoAPI();
+    private CovierteDatos conversor = new CovierteDatos();
     public void ejecutaMenu(){
-        var json = consumoApi.obtenerDatos(URL_BASE);|
+        var json = consumoAPI.obtenerDatos(URL_BASE);
+        System.out.println(json);
+        var datos = conversor.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
         
     }
 }
